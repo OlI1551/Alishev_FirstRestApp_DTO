1542f11d00d17e79e8348315bdce5ac7da454082
OlI1551 <I15510000000@gmail.com>
2024-09-19 00:52:23 +0800

    first commit

---
+++ src/main/java/ru/alishev/springcourse/FirstRestApp/controllers/PeopleController.java
@@ -0,0 +1,102 @@
+package ru.alishev.springcourse.FirstRestApp.controllers;
+
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import ru.alishev.springcourse.FirstRestApp.dto.PersonDTO;
+import ru.alishev.springcourse.FirstRestApp.util.PersonNotCreatedException;
+import ru.alishev.springcourse.FirstRestApp.util.PersonNotFoundException;
+import ru.alishev.springcourse.FirstRestApp.models.Person;
+import ru.alishev.springcourse.FirstRestApp.services.PeopleService;
+import ru.alishev.springcourse.FirstRestApp.util.PersonErrorResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PeopleService peopleService;
+
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public PeopleController(PeopleService peopleService,
+                            ModelMapper modelMapper) {
+        this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping()
+    public List<PersonDTO> getPeople() {
+        return peopleService.findAll().stream()
+                .map(this::convertToPersonDTO)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO getPerson(@PathVariable("id") int id) {
+        // Статус - 200
+        return convertToPersonDTO(peopleService.findOne(id)); // Jackson конвертирует в JSON
+
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO,
+                                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder errorMsg = new StringBuilder();
+
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                errorMsg.append(error.getField())
+                        .append(" - ").append(error.getDefaultMessage())
+                        .append(";");
+            }
+
+            throw new PersonNotCreatedException(errorMsg.toString());
+        }
+
+        peopleService.save(convertToPerson(personDTO));
+
+        // отправляем HTTP ответ с пустым телом и со статусом 200
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<PersonErrorResponse> handleException(PersonNotFoundException e) {
+        PersonErrorResponse response = new PersonErrorResponse(
+                "Person with this id wasn't found!",
+                System.currentTimeMillis()
+        );
+        // В HTTP ответе тело ответа (response) и статус в заголовке
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND); // NOT_FOUND - 404 статус
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<PersonErrorResponse> handleException(PersonNotCreatedException e) {
+        PersonErrorResponse response = new PersonErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+        // В HTTP ответе тело ответа (response) и статус в заголовке
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST); // статус 400
+    }
+
+    private Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
+    }
+
+    private PersonDTO convertToPersonDTO(Person person) {
+        return modelMapper.map(person, PersonDTO.class);
+    }
+
+}
